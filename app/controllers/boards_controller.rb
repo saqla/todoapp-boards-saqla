6c51b1d5b675ccf09a914f30d54d9e088179aead
saqla <whitecross8fortune8fatedress@gmail.com>
2020-09-01 20:44:29 +0900

    ボード一覧の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,5 @@
 class BoardsController < ApplicationController
   def index
-    @boards = Board.first
+    @boards = Board.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Board.create({title: '新しい記事だよ', content: '素晴らしい記事です'})
+Board.create({title: '新し~記事だよ~', content: '素晴らし~事で~す'})
