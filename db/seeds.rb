ae880d6a3ba1063c9131bddf5ff25ff52c7cf16e
saqla <whitecross8fortune8fatedress@gmail.com>
2020-09-01 20:53:52 +0900

    ダミーデータの作成 - Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Board.create({title: '新しい記事だよ', content: '素晴らしい記事です'})
-Board.create({title: '新し~記事だよ~', content: '素晴らし~事で~す'})
+10.times do
+  Board.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
