a685376d77b457bdf7e78570a734055117eb45f9
saqla <whitecross8fortune8fatedress@gmail.com>
2020-09-07 13:33:48 +0900

    show-board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,36 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show]
   before_action :authenticate_user!
 
   def index
     @boards = Board.all
   end
 
-  def new
+  def show
+    @board = Board.find(params[:id])
+    # @tasks = @board.tasks
   end
+
+  def new
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
 end
